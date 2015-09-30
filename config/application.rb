47591236fdc5e836c299ba8a055b5a5e2250fb88
KamiuraAkiyuki <mxg7y3ansl@gmail.com>
2015-09-30 21:58:00 +0000

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module SampleApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # for bootstrap precompiling for asset pipeline
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
