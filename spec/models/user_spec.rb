5c21084c8c7f7288260de5159e10aa84e91a1e6d
KamiuraAkiyuki <mxg7y3ansl@gmail.com>
2015-10-08 13:38:34 +0000

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,18 +18,26 @@ describe User do
 
 	it { should be_valid }
 
+	# -------------------------------------------------------------------------------
+	# name
+	# -------------------------------------------------------------------------------
+
 	describe "when name is not present" do
 		before { @user.name = " " }
 		it { should_not be_valid }
 	end
 
-	describe "when email is not present" do 
-		before { @user.email = " " }
+	describe "when name is too long" do
+		before { @user.name = "a" * 51 }
 		it { should_not be_valid }
 	end
 
-	describe "when name is too long" do
-		before { @user.name = "a" * 51 }
+	# -------------------------------------------------------------------------------
+	# email
+	# -------------------------------------------------------------------------------
+
+	describe "when email is not present" do 
+		before { @user.email = " " }
 		it { should_not be_valid }
 	end
 
@@ -63,6 +71,20 @@ describe User do
 
 	    it { should_not be_valid }
 	end
+
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixed_case_email.downcase
+		end
+	end
+
+	# -------------------------------------------------------------------------------
+	# password
+	# -------------------------------------------------------------------------------
 
 	describe "when password is not present" do
 	    before do
